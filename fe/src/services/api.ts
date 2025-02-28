c65e229e5c7096b374b4a763cc729b21ce1aa944
demaceo <hdemaceo@gmail.com>
2025-02-28 14:44:07 -0700

    use env var

--- fe/src/services/api.ts
+++ fe/src/services/api.ts
@@ -1,8 +1,11 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const fetchOfficials = async (address: string) => {
-    const response = await fetch("http://localhost:5000/api/officials", {
+    const response = await fetch(`${API_BASE_URL}/api/officials`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ address }),
     });
+
     return response.json();
 };
