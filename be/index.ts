30cafb15e0ce0749814c4c35ee469192600a79ac
demaceo <hdemaceo@gmail.com>
2025-03-07 15:43:10 -0700

    add openStatesRoutes

--- be/index.ts
+++ be/index.ts
@@ -1,23 +1,21 @@
 import express from "express";
-// const {express} = require("express");
 import cors from "cors";
-// const { cors } = require("cors");
-// const { dotenv } = require("dotenv")
 import dotenv from "dotenv";
 import officialsRoutes from "./routes/officialsRoutes.js";
 import emailRoutes from "./routes/emailRoutes.js";
 import moderationRoutes from "./routes/moderationRoutes.js";
 import chatbotRoutes from "./routes/chatbotRoutes.js";
-// const {officialsRoutes} = require("./routes/officialsRoutes");
-// const {emailRoutes} = require("./routes/emailRoutes");
-// const {moderationRoutes} = require("./routes/moderationRoutes");
-// const {chatbotRoutes} = require("./routes/chatbotRoutes");
+import openStatesRoutes from "./routes/openStatesRoutes.js";
+
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+
+app.use("/api/openstates", openStatesRoutes);
 
 app.use("/api/officials", officialsRoutes);
 app.use("/api/email", emailRoutes);
