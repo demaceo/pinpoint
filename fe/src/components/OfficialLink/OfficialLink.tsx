03ae4f6917eaa9a4fb25fea21ae491afda019282
demaceo <hdemaceo@gmail.com>
2025-03-13 15:12:07 -0600

    checkboxes instead of bullet points

--- fe/src/components/OfficialLink/OfficialLink.tsx
+++ fe/src/components/OfficialLink/OfficialLink.tsx
@@ -5,6 +5,7 @@ const OfficialLink: React.FC<OfficialLinkProps> = ({
   index,
   official,
   onSelect,
+  isChecked,
 }) => {
 
   const partyInitial = official.party?.charAt(0) || "N/A";
@@ -14,10 +15,23 @@ const OfficialLink: React.FC<OfficialLinkProps> = ({
   };
 
   return (
-    <li key={index} className="list-item">
-      <span onClick={handleClick}>{official.name}</span> -{" "}
-      {official.current_role?.title} ({partyInitial})
-    </li>
+    // <li key={index} className="list-item">
+    //   <span onClick={handleClick}>{official.name}</span> -{" "}
+    //   {official.current_role?.title} ({partyInitial})
+    // </li>
+    <div key={index} className="checkbox-container">
+      <input
+        type="checkbox"
+        id={`official-${index}`}
+        checked={isChecked} 
+        onChange={onSelect}
+      />
+      <label htmlFor={`official-${index}`}>
+        <span onClick={handleClick}>{official.name}</span> {""}-{" "}
+        {official.current_role?.title}{" "}
+        <span id={partyInitial}>({partyInitial})</span>
+      </label>
+    </div>
   );
 };
 
