25ce9d43d434d469375be81712cfe4b025888995
demaceo <hdemaceo@gmail.com>
2025-03-17 14:12:37 -0600

    relocate StateSVGs folder to assets

--- fe/src/utils/StateSvgsLoader.ts
+++ fe/src/utils/StateSvgsLoader.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // This imports every .svg in ../StateSVGs/ eagerly (i.e. at build time)
-const modules = import.meta.glob<string>("../StateSVGs/*.svg", { eager: true });
+const modules = import.meta.glob<string>("../assets/StateSVGs/*.svg", { eager: true });
 const stateSvgMap: Record<string, string> = {};
 
 for (const path in modules) {
