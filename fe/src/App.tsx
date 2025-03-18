4cf27142e1107b3903ed3dc7e4cd967d4514c4fb
demaceo <hdemaceo@gmail.com>
2025-03-18 13:10:50 -0600

    replace Home and OfficialsByLocation pages with Landing page

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home.tsx";
+import Landing from "./pages/Landing/Landing.tsx";
 import "./App.css";
-import OfficialsByLocation from "./pages/OfficialsByLocation/OfficialsByLocation.tsx";
 import Country from "./pages/Country/Country.tsx";
 import NavBar from "./components/NavBar/NavBar.tsx";
 import Footer from "./components/Footer/Footer.tsx";
-// import NeonSmoke from "./components/LoadingSpinner/NeonSmoke.tsx";
-import RandomLetter from "./components/LoadingSpinner/RandomLetter.tsx";
+import NeonSmoke from "./components/LoadingSpinner/NeonSmoke.tsx";
 
 const App: React.FC = () => {
+  const HomeLayout = () => (
+    <div className="app-container">
+      <NavBar />
+      <div className="main-content">
+        <Landing />
+      </div>
+      <Footer />
+    </div>
+  );
   return (
     <Router>
       <Routes>
@@ -19,37 +26,14 @@ const App: React.FC = () => {
             <div className="app-container">
               <NavBar />
               <div className="main-content">
-                <RandomLetter />
-               </div>
-              <Footer />
-            </div> 
-          }
-        />
-        <Route
-          path="/"
-          element={
-            <div className="app-container">
-              <NavBar />
-              <div className="main-content">
-                <Home />
+                <NeonSmoke />
               </div>
               <Footer />
             </div>
           }
         />
-        <Route
-          path="/yourofficials"
-          element={
-            <div className="app-container">
-              <NavBar />
-              <div className="main-content">
-                <OfficialsByLocation />
-              </div>
-              <Footer />
-            </div>
-          }
-        />
-
+        <Route path="/" element={<HomeLayout />} />
+        <Route path="/near-you" element={<HomeLayout />} />
         <Route
           path="/map"
           element={
---
+++ fe/src/pages/Landing/Landing.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState, lazy, Suspense } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Landing.css";
+import Welcome from "../../components/Welcome/Welcome.tsx";
+const Officials = lazy(() => import("../Officials/Officials"));
+
+const Landing: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const currentEndpoint = useLocation();
+  const isNearYouPage = currentEndpoint.pathname === "/near-you";
+  const [headerText, setHeaderText] = useState<string>("");
+  const [linkText, setLinkText] = useState<string>("");
+
+  useEffect(() => {
+    if (isNearYouPage) {
+      setHeaderText("Elected Representatives Near You");
+      setLinkText("Find Elected Representatives by State");
+    } else {
+      setHeaderText("Elected Representatives in");
+      setLinkText("Find Elected Representatives Near You");
+    }
+  }, [isNearYouPage]);
+
+
+  return (
+    <div className={`${loading ? "loading-page" : "home-officials-page"}`}>
+      <div className={`${loading ? "loading-container" : "home-container"}`}>
+        <Welcome />
+        <Link to={isNearYouPage ? "/" : "/near-you"} className="link-officials">
+          {linkText}
+        </Link>
+      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Officials
+          isNearYouPage={isNearYouPage}
+          setLoading={setLoading}
+          headerText={headerText}
+        />
+      </Suspense>
+    </div>
+  );
+};
+
+export default Landing;
