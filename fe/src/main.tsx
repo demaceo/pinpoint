4ec9f2e1b3a16b4d02632149b39c97ff637063f3
demaceo <hdemaceo@gmail.com>
2025-03-21 17:04:47 -0600

    install Vercel Analytics

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import "./styles/theme.css";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
