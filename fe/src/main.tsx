54bda727e033afa3c66493b7d618c5fa0a5e68d2
demaceo <hdemaceo@gmail.com>
2025-03-21 19:23:08 -0600

    remove Speed Insights

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import "./styles/theme.css";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Analytics />
-    <SpeedInsights />
     <App />
   </React.StrictMode>
 );
