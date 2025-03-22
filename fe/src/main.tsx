c8080939e74bf40473fa73125d75bf33b220583a
demaceo <hdemaceo@gmail.com>
2025-03-21 18:52:55 -0600

    install Speed Insights vercel

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -4,10 +4,12 @@ import App from "./App.jsx";
 // import "./styles/theme.css";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Analytics />
+    <SpeedInsights />
     <App />
   </React.StrictMode>
 );
